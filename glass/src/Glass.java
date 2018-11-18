5e1b28478c9cd21974b304d482f5d0ba44a0ee80
HarisMuflizovic <mufle.h@outlook.com>
2018-11-18 23:34:12 +0100

    Create toString method with message

--- glass/src/Glass.java
+++ glass/src/Glass.java
@@ -1,15 +1,43 @@
 public class Glass {
 
     private String liquid;
-    private int amount;
-    private int max;
+    private Integer amount;
+    private Integer max;
 
     public Glass(){
         super();
     }
-    public Glass(int max){
+
+    public Glass(Integer max){
         this.max = max;
+        this.liquid = "Watter";
+        this.amount = 0;
+    }
+    public Integer getAmount() {
+        return amount;
+    }
+    public String getLiquid(){
+        return liquid;
+    }
+    public Integer getMax(){
+        return max;
     }
 
+    public void setAmount(Integer amount){
+        this.amount = amount;
+    }
+    public void setMax(Integer max){
+        this.max = max;
+    }
+    public void setLiquid(String liquid){
+        this.liquid = liquid;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Glass type is %s, amount is %d, max is %d.", liquid, amount, max);
+
+    }
 }
+
 
--- glass/src/Main.java
+++ glass/src/Main.java
@@ -1,9 +1,19 @@
+import jdk.nashorn.internal.objects.annotations.Setter;
+
 public class Main {
 
     public static void main(String[] args) {
 
-     Glass glass = new glass;
+        Glass myGlass = new Glass(5);
+        Glass secondGlass = new Glass();
 
-        System.out.println("Kolicina vode u casi je a maksimalna kolicina je ");
+        myGlass.setAmount(3);
+        
+        secondGlass.setAmount(2);
+        secondGlass.setLiquid("coca cola");
+        secondGlass.setMax(5);
+
+        System.out.println(myGlass.toString());
+        System.out.println(secondGlass.toString());
     }
 }
